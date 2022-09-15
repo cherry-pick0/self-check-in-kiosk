fdd062601c03b52d8d51a49a9071491b1756b0f0
cherry-pick0 <bozic.jerneja@gmail.com>
2022-09-15 12:24:36 +0100

    Update form models.

--- kiosk/data/forms/models.py
+++ kiosk/data/forms/models.py
@@ -18,13 +18,31 @@ What:
     FormQuestion
 Attributes:
     kiosk_form: One form can have multiple questions
+    title: Title of the question
+    question_type: Type of a question (input, textarea, select, checkbox)
 """
+
+INPUT = "I"
+TEXTAREA = "T"
+SELECT = "S"
+CHECKBOX = "C"
+QUESTION_TYPE_CHOICES = (
+    (INPUT, "Input"),
+    (TEXTAREA, "textarea"),
+    (SELECT, "Select"),
+    (CHECKBOX, "Checkbox"),
+)
 
 
 class FormQuestion(models.Model):
     kiosk_form = models.ForeignKey(
         "forms.KioskForm", on_delete=models.CASCADE, null=False
     )
+    title = models.CharField(max_length=64, null=False)
+    question_type = models.CharField(
+        max_length=1, choices=QUESTION_TYPE_CHOICES, default=INPUT
+    )
+
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -39,8 +57,12 @@ Attributes:
 
 
 class FormAnswer(models.Model):
+    kiosk_registration = models.ForeignKey(
+        "registrations.KioskRegistration", on_delete=models.CASCADE, null=False
+    )
     form_question = models.ForeignKey(
         "forms.KioskForm", on_delete=models.CASCADE, null=False
     )
+    value = models.JSONField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
